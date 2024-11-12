0ffe042c81d3dbabfb9ff845a9f2c81e0817299a
Michael Jiang <mjiang@lcs.on.ca>
2024-11-12 14:26:28 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Michael on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
